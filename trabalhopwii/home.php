1daf5db42f26181dbc62a951114839700bf0e32b
Érika <128494386+erikadsz@users.noreply.github.com>
2024-04-01 23:11:22 -0300

    Add files via upload

---
+++ trabalhopwii/home.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
+
+
+    <title> PetCare - O serviço de gestão para sua clínica. </title>
+    <link rel="stylesheet" href="styles.css">
+    <script src="scripts.js"></script>
+
+</head>
+<body>
+<menu>
+<ul>
+    <li> <img src="./img/capa2.png" class="top"/> </li>
+    <li class="liOne"> <a href="#"> Início </a> </li>
+
+    <li class="liOne"> <a href="#"> Planos </a> 
+    <ul class="dropdown">
+    <li> <a href="#"> Simples </a> </li>
+    <li> <a href="#"> Premium </a> </li>
+    <li> <a href="#"> Gold </a> </li>
+    </ul>
+    </li>
+
+    <li class="liOne"> <a href="#"> Controle de Estoque </a> 
+    <ul class="dropdown">
+    <li> <a href="#"> Vacinas </a> </li>
+    <li> <a href="#"> Remédios </a> </li>
+    </ul>
+    </li>
+
+ 
+
+ </ul>
+ </menu> 
+
+ <div class="container-slider">
+    <button id="prev-button"><img src="./img/arrow.png" alt="prev-button"></button>
+    <div class="container-images">
+      <img src="./img/coverFour.png" alt="image1" class="slider on">
+      <img src="./img/coverSix.png" alt="image2" class="slider">
+      <img src="./img/cp.png" alt="image3" class="slider">
+
+</div>
+    <button id="next-button"><img src="./img/arrow.png" alt="next-button"></button>
+</div>
+<div class="card">
+    fazer um card animado aqui
+</div>
+<div class="card">
+</div>
+<div class="card">
+</div><div class="card">
+</div>
+  <div class="content">
+    <h1></h1>
+  </div>
+
+<footer>
+</footer>
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+</body>
+</html
